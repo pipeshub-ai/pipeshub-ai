96d9d89f3cdb3edb8209bdb78cd1ffb855e47250
khushijainslack <khushi.jain@pipeshub.com>
2025-04-27 17:57:25 +0530

    metric collection flag changes

---
+++ services/nodejs/apps/src/modules/user_management/services/cm.service.ts
@@ -0,0 +1,47 @@
+import axios, { AxiosError } from 'axios';
+import { injectable } from 'inversify';
+import { InternalServerError } from '../../../libs/errors/http.errors';
+
+interface ConfigManagerResponse {
+  statusCode: number;
+  data: any;
+}
+@injectable()
+export class ConfigurationManagerService {
+  constructor() {}
+
+  async setConfig(
+    cmBackendUrl: string,
+    configUrlPath: string,
+    scopedToken: string,
+    body: Record<string, any>,
+  ): Promise<ConfigManagerResponse> {
+    try {
+      const config = {
+        method: 'post' as const,
+        url: `${cmBackendUrl}/${configUrlPath}`,
+        headers: {
+          Authorization: `Bearer ${scopedToken}`,
+          'Content-Type': 'application/json',
+        },
+        data: body,
+      };
+
+      const response = await axios(config);
+      return { statusCode: 200, data: response.data };
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        throw new AxiosError(
+          error.response?.data?.message || 'Error setting config',
+          error.code,
+          error.config,
+          error.request,
+          error.response,
+        );
+      }
+      throw new InternalServerError(
+        error instanceof Error ? error.message : 'Unexpected error occurred',
+      );
+    }
+  }
+}
