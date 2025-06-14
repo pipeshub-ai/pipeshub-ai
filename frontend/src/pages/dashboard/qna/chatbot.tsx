94f4dbc7aae5a6f9fb1fd30580d8f21389dcffc1
vishwjeet <vishwjeet.pawar@pipeshub.com>
2025-06-15 02:28:39 +0530

    Unused configuration stepper removed

--- frontend/src/pages/dashboard/qna/chatbot.tsx
+++ frontend/src/pages/dashboard/qna/chatbot.tsx
@@ -14,8 +14,7 @@ import { useAuthContext } from 'src/auth/hooks';
 import { AuthProvider } from 'src/auth/context/jwt';
 
 import FullNameDialog from '../components/full-name-dialog';
-import ConfigurationStepper from '../configuration-stepper/configuration-stepper';
-import FullyUniversalStepper from '../configuration-stepper/components/stepper';
+import OnBoardingStepper from '../configuration-stepper/components/stepper';
 
 // ----------------------------------------------------------------------
 
@@ -199,7 +198,7 @@ function PageContent() {
   return (
     <>
       {/* Only render ConfigurationStepper if user is admin */}
-      {isAdmin && <FullyUniversalStepper open={configDialog} onClose={handleClose} />}
+      {isAdmin && <OnBoardingStepper open={configDialog} onClose={handleClose} />}
 
       {/* Full Name Dialog - shown to all users */}
       <FullNameDialog
