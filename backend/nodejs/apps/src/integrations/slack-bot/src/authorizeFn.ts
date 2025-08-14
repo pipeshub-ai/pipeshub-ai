07401508e478ddbe62591684f911ce12418a5a4a
kaushal1999 <kaushalwaghela1999@gmail.com>
2025-08-14 13:09:05 +0530

    Add feature: slack integration

---
+++ backend/nodejs/apps/src/integrations/slack-bot/src/authorizeFn.ts
@@ -0,0 +1,15 @@
+interface AuthorizeParams {
+  teamId: string;
+}
+
+interface AuthorizationResult {
+  botToken: string;
+}
+
+const authorizeFn =
+  async ({}: AuthorizeParams): Promise<AuthorizationResult> => {
+    return { botToken: process.env.BOT_TOKEN || '' };
+    // Add custom authorization logic if needed
+  };
+
+export default authorizeFn;
---
+++ backend/nodejs/apps/src/integrations/slack-bot/src/slackApp.ts
@@ -0,0 +1,21 @@
+import { App } from '@slack/bolt';
+
+import receiver from './receiver';
+
+import authorizeFn from './authorizeFn';
+
+let app: App | undefined;
+
+if (!app) {
+  app = new App({
+    authorize: authorizeFn as any, // Type assertion to resolve compatibility issue
+
+    signingSecret: process.env.SLACK_SIGNING_SECRET || '',
+
+    receiver: receiver,
+
+    socketMode: false,
+  });
+}
+
+export default app!;
