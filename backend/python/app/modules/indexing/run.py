1f867b9c8ce2c004bf4b19f9c69209eda057df60
kaushal1999 <kaushalwaghela1999@gmail.com>
2025-11-21 19:58:13 +0530

    ruff check

--- backend/python/app/modules/indexing/run.py
+++ backend/python/app/modules/indexing/run.py
@@ -661,7 +661,7 @@ class IndexingPipeline:
 
             try:
                 await self.arango_service.delete_nodes(keys=[virtual_record_id], collection=CollectionNames.VIRTUAL_RECORD_TO_DOC_ID_MAPPING.value)
-                
+
                 filter_dict = await self.vector_db_service.filter_collection(
                     must={"virtualRecordId": virtual_record_id}
                 )
@@ -689,7 +689,7 @@ class IndexingPipeline:
 
                 if ids:
                     await self.vector_store.adelete(ids=ids)
-                
+
 
                 self.logger.info(
                     f"✅ Successfully deleted embeddings for record {record_id}"
