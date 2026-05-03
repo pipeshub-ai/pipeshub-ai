f15f291852fcf0adc3eca927e3b07adcdfcd1611
kushagra1122 <kushagratiwari24@gmail.com>
2026-05-03 09:59:57 +0530

    refactor(tests): update connector utility imports and enhance test coverage for error handling

--- backend/nodejs/apps/tests/modules/tokens_manager/controllers/oauth.controllers.test.ts
+++ backend/nodejs/apps/tests/modules/tokens_manager/controllers/oauth.controllers.test.ts
@@ -1,7 +1,7 @@
 import 'reflect-metadata'
 import { expect } from 'chai'
 import sinon from 'sinon'
-import * as connectorUtils from '../../../../src/modules/tokens_manager/utils/connector-service-http'
+import * as connectorUtils from '../../../../src/modules/tokens_manager/utils/connector.utils'
 import * as connectorControllers from '../../../../src/modules/tokens_manager/controllers/connector.controllers'
 import {
   getOAuthConfigRegistry,
