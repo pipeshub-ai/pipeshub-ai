eac9860da60662138d1f53145de502f744128824
kushagra1122 <kushagratiwari24@gmail.com>
2026-05-03 09:59:57 +0530

    refactor(tests): update connector utility imports and enhance test coverage for error handling

--- backend/nodejs/apps/tests/modules/toolsets/routes/toolsets_routes.test.ts
+++ backend/nodejs/apps/tests/modules/toolsets/routes/toolsets_routes.test.ts
@@ -1,7 +1,7 @@
 import 'reflect-metadata'
 import { expect } from 'chai'
 import sinon from 'sinon'
-import * as connectorUtils from '../../../../src/modules/tokens_manager/utils/connector-service-http'
+import * as connectorUtils from '../../../../src/modules/tokens_manager/utils/connector.utils'
 import { createToolsetsRouter } from '../../../../src/modules/toolsets/routes/toolsets_routes'
 
 // ---------------------------------------------------------------------------
