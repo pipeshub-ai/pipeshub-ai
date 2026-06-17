474c65a6ed0fa3499d1beb2af86cccaddc2b942d
Tushar Sharma <stushar695@gmail.com>
2026-06-17 16:24:09 +0530

    minor ut fix

--- backend/python/app/connectors/sources/gitlab/__init__.py
+++ backend/python/app/connectors/sources/gitlab/__init__.py
@@ -1,10 +1,1 @@
-"""
-GitLab connector package (staging directory for the refactored connector).
-
-After validation this directory will replace app/connectors/sources/gitlab/.
-"""
-
-from app.connectors.sources.gitlab.connector import GitLabConnector
-from app.connectors.sources.gitlab.constants import GITLAB_CLOUD_URL
-
-__all__ = ["GitLabConnector", "GITLAB_CLOUD_URL"]
+"""GitLab connector package."""
