d2b1367c175e14de4639bd8bf8b3a33262866de1
abhishekgml1991 <abhishek@contextualml.com>
2026-09-17 22:43:06 -0700

    User activities test

--- backend/nodejs/apps/tests/libs/utils/userActivities.utils.test.ts
+++ backend/nodejs/apps/tests/libs/utils/userActivities.utils.test.ts
@@ -1,7 +1,7 @@
 import 'reflect-metadata'
 import { expect } from 'chai'
 import sinon from 'sinon'
-import { userActivitiesType } from '../../../src/libs/utils/userActivities.utils'
+import { SESSION_INVALIDATING_ACTIVITIES, userActivitiesType } from '../../../src/libs/utils/userActivities.utils'
 
 describe('userActivities.utils', () => {
   afterEach(() => {
@@ -45,8 +45,18 @@ describe('userActivities.utils', () => {
       expect(userActivitiesType.ROLE_CHANGED).to.equal('ROLE CHANGED')
     })
 
-    it('should have exactly 9 activity types', () => {
-      expect(Object.keys(userActivitiesType)).to.have.length(9)
+    it('should have ACCOUNT_BLOCKED activity type', () => {
+      expect(userActivitiesType.ACCOUNT_BLOCKED).to.equal('ACCOUNT BLOCKED')
+    })
+
+    it('should have exactly 10 activity types', () => {
+      expect(Object.keys(userActivitiesType)).to.have.length(10)
+    })
+
+    it('should include ACCOUNT_BLOCKED in session-invalidating activities', () => {
+      expect(SESSION_INVALIDATING_ACTIVITIES).to.include(
+        userActivitiesType.ACCOUNT_BLOCKED,
+      )
     })
 
     it('should have unique values for all activity types', () => {
